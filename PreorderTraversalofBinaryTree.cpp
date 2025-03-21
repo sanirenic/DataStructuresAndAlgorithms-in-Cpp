c9be3a7fd92c7e43ea5d36274fa6a952b394cffe
sanjana <sanjanameena1904@gmail.com>
2025-03-21 19:00:00 +0530

    sdufdsuhirk

---
+++ PreorderTraversalofBinaryTree.cpp
@@ -0,0 +1,61 @@
+/*Problem Statement: Given the root of a Binary Tree, 
+write a recursive function that returns an array containing the preorder traversal of the tree.*/
+
+                            
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Node structure for
+// the binary tree
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    // Constructor to initialize
+    // the node with a value
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+
+void preorder(Node* root, vector<int> &arr){
+    
+    if(root == nullptr){
+        return;
+    }
+    arr.push_back(root->data);
+    preorder(root->left, arr);
+    preorder(root->right, arr);
+}
+
+vector<int> preOrder(Node* root){
+    vector<int> arr;
+    preorder(root, arr);
+    return arr;
+}
+
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    vector<int> result = preOrder(root);
+
+    
+    cout << "Preorder Traversal: ";
+    
+    for(int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+
+    return 0;
+}
+
+                            
+                        
