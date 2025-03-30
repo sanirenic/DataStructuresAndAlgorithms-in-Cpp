9633006b0300ed6799621f77364f667b33ecfbe5
sanjana <sanjanameena1904@gmail.com>
2025-03-30 16:10:18 +0530

    ggggghhhh

--- MaxDepthOfBT.cpp
+++ MaxDepthOfBT.cpp
@@ -1,51 +1,39 @@
-                            
+/*Problem Statement: Given the root of a Binary Tree, return the height of the tree. 
+The height of the tree is equal to the number of nodes on the longest path from root to a leaf.*/
+
+
 #include <iostream>
 #include <vector>
 #include <stack>
 
 using namespace std;
 
-// Node structure for the binary tree
 struct Node {
     int data;
     Node* left;
     Node* right;
-    // Constructor to initialize
-    // the node with a value
+
     Node(int val) : data(val), left(nullptr), right(nullptr) {}
 };
 
 class Solution{
 public:
-    // Function to find the
-    // maximum depth of a binary tree
+
     int maxDepth(Node* root){
-        // If the root is NULL
-        // (empty tree), depth is 0
+
         if(root == NULL){
             return 0;
         }
         
-        // Recursive call to find the
-        // maximum depth of the left subtree
         int lh = maxDepth(root->left);
-        
-        // Recursive call to find the
-        // maximum depth of the right subtree
         int rh = maxDepth(root->right);
-        
-        // Return the maximum depth of the
-        // tree, adding 1 for the current node
+
         return 1 + max(lh, rh);
     }
 };
 
-
-
-
-// Main function
 int main() {
-    // Creating a sample binary tree
+  
     Node* root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
