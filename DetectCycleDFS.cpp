fdac969f1fdf582435104aef8e489e54ab0c0128
sanirenic <sanjanameena1904@gmail.com>
2025-06-21 22:27:46 +0530

    DetectCycleDFS2

--- DetectCycleDFS.cpp
+++ DetectCycleDFS.cpp
@@ -6,23 +6,23 @@ class Solution {
   private: 
     bool dfs(int node, int parent, int vis[], vector<int> adj[]) {
         vis[node] = 1; 
-        // visit adjacent nodes
+        
         for(auto adjacentNode: adj[node]) {
-            // unvisited adjacent node
+           
             if(!vis[adjacentNode]) {
                 if(dfs(adjacentNode, node, vis, adj) == true) 
                     return true; 
             }
-            // visited node but not a parent node
+            
             else if(adjacentNode != parent) return true; 
         }
         return false; 
     }
   public:
-    // Function to detect cycle in an undirected graph.
+    
     bool isCycle(int V, vector<int> adj[]) {
        int vis[V] = {0}; 
-       // for graph with connected components 
+       
        for(int i = 0;i<V;i++) {
            if(!vis[i]) {
                if(dfs(i, -1, vis, adj) == true) return true; 
@@ -33,8 +33,7 @@ class Solution {
 };
 
 int main() {
-    
-    // V = 4, E = 2
+   
     vector<int> adj[4] = {{}, {2}, {1, 3}, {2}};
     Solution obj;
     bool ans = obj.isCycle(4, adj);
