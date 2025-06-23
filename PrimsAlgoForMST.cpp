fad7f6b206143f706500ee31fee5724abefd0b9f
sanirenic <sanjanameena1904@gmail.com>
2025-06-23 18:52:38 +0530

    pamstcpp2

--- PrimsAlgoForMST.cpp
+++ PrimsAlgoForMST.cpp
@@ -4,14 +4,13 @@ using namespace std;
 class Solution
 {
 public:
-	//Function to find sum of weights of edges of the Minimum Spanning Tree.
 	int spanningTree(int V, vector<vector<int>> adj[])
 	{
 		priority_queue<pair<int, int>,
 		               vector<pair<int, int> >, greater<pair<int, int>>> pq;
 
 		vector<int> vis(V, 0);
-		// {wt, node}
+		
 		pq.push({0, 0});
 		int sum = 0;
 		while (!pq.empty()) {
@@ -21,7 +20,7 @@ public:
 			int wt = it.first;
 
 			if (vis[node] == 1) continue;
-			// add it to the mst
+			
 			vis[node] = 1;
 			sum += wt;
 			for (auto it : adj[node]) {
