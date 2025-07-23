5c7808906a345b5f11629aa112f6aa5563a14ebd
sanirenic <sanjanameena1904@gmail.com>
2025-07-23 18:21:16 +0530

    MinInsertionToMakeStringPalindrome2cppp

--- MinInsertionToMakeStringPalindrome.cpp
+++ MinInsertionToMakeStringPalindrome.cpp
@@ -1,15 +1,12 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function to calculate the length of the Longest Common Subsequence
 int lcs(string s1, string s2) {
     int n = s1.size();
     int m = s2.size();
 
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
 
-    // Initialize the first row and first column to 0
     for (int i = 0; i <= n; i++) {
         dp[i][0] = 0;
     }
