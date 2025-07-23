3c0f6ddf485da1359707d38a45ee36ab37b7c1ff
sanirenic <sanjanameena1904@gmail.com>
2025-07-23 21:07:39 +0530

    ImplementTriecppp

---
+++ ImplementTrie.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// TrieNode definition
+class TrieNode {
+public:
+    TrieNode* children[26]; // for a-z
+    int wordCount;          // number of times this word ends here
+    int prefixCount;        // number of words passing through this node
+
+    TrieNode() {
+        wordCount = 0;
+        prefixCount = 0;
+        for (int i = 0; i < 26; i++)
+            children[i] = nullptr;
+    }
+
+    ~TrieNode() {
+        // Recursively delete all children
+        for (int i = 0; i < 26; i++) {
+            if (children[i])
+                delete children[i];
+        }
+    }
+};
+
+// Trie definition
+class Trie {
+private:
+    TrieNode* root;
+
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    // Insert a word into the trie
+    void insert(string word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int i = ch - 'a';
+            if (node->children[i] == nullptr)
+                node->children[i] = new TrieNode();
+            node = node->children[i];
+            node->prefixCount++; // this prefix is used
+        }
+        node->wordCount++; // word ends here
+    }
+
+    // Count words equal to a given word
+    int countWordsEqualTo(string word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int i = ch - 'a';
+            if (node->children[i] == nullptr)
+                return 0;
+            node = node->children[i];
+        }
+        return node->wordCount;
+    }
+
+    // Count words starting with a given prefix
+    int countWordsStartingWith(string prefix) {
+        TrieNode* node = root;
+        for (char ch : prefix) {
+            int i = ch - 'a';
+            if (node->children[i] == nullptr)
+                return 0;
+            node = node->children[i];
+        }
+        return node->prefixCount;
+    }
+
+    // Erase a word from the trie (one occurrence)
+    void erase(string word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int i = ch - 'a';
+            TrieNode* next = node->children[i];
+            next->prefixCount--;  // one fewer word passes through here
+            node = next;
+        }
+        node->wordCount--; // one fewer word ends here
+    }
+
+    // Destructor to release memory
+    ~Trie() {
+        delete root;
+    }
+};
