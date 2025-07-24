abc9f800c166d6c29941814eeddea7117f74b713
sanirenic <sanjanameena1904@gmail.com>
2025-07-24 17:05:04 +0530

    MinInsertDeletToConvertStringcppp

---
+++ MinInsertDeletToConvertString.cpp
@@ -0,0 +1,51 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to calculate the length of the Longest Common Subsequence
+int lcs(string s1, string s2) {
+    int n = s1.size();
+    int m = s2.size();
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+
+    // Initialize the first row and first column to 0
+    for (int i = 0; i <= n; i++) {
+        dp[i][0] = 0;
+    }
+    for (int i = 0; i <= m; i++) {
+        dp[0][i] = 0;
+    }
+
+    for (int ind1 = 1; ind1 <= n; ind1++) {
+        for (int ind2 = 1; ind2 <= m; ind2++) {
+            if (s1[ind1 - 1] == s2[ind2 - 1])
+                dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+            else
+                dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+        }
+    }
+
+    return dp[n][m];
+}
+
+// Function to calculate the minimum operations required to convert str1 to str2
+int canYouMake(string str1, string str2) {
+    int n = str1.size();
+    int m = str2.size();
+    
+    // Calculate the length of the longest common subsequence between str1 and str2
+    int k = lcs(str1, str2);
+    
+    // Calculate the minimum operations required to convert str1 to str2
+    return (n - k) + (m - k);
+}
+
+int main() {
+    string str1 = "abcd";
+    string str2 = "anc";
+    
+    // Call the canYouMake function and print the result
+    cout << "The Minimum operations required to convert str1 to str2: " << canYouMake(str1, str2);
+    return 0;
+}
