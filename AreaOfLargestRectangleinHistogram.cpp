ea2bb12b0c4dc038521a99888357759e59a33625
sanirenic <sanjanameena1904@gmail.com>
2025-08-08 21:08:28 +0530

    AreaOfLargestRectangleinHistogramcpp

---
+++ AreaOfLargestRectangleinHistogram.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int largestRectangleArea(vector<int>& heights) {
+    int n = heights.size();
+    stack<int> st;
+    int maxArea = 0;
+
+    for (int i = 0; i <= n; ++i) {
+        int currHeight = (i == n) ? 0 : heights[i];
+
+        while (!st.empty() && currHeight < heights[st.top()]) {
+            int height = heights[st.top()];
+            st.pop();
+            int width = st.empty() ? i : i - st.top() - 1;
+            maxArea = max(maxArea, height * width);
+        }
+
+        st.push(i);
+    }
+
+    return maxArea;
+}
+
+int main() {
+    vector<int> heights = {2, 1, 5, 6, 2, 3};
+    cout << "Max area: " << largestRectangleArea(heights) << endl;
+    return 0;
+}
