5602c013582a2547a15b7c84b0b0ac6914bc2b61
sanirenic <sanjanameena1904@gmail.com>
2025-08-12 15:46:25 +0530

    NextPermutationcppp

---
+++ NextPermutation.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void nextPermutation(vector<int> &arr){
+    int n=arr.size();
+    int i=n-2;
+
+    while(i>=0 && arr[i]>=arr[i+1]) i--;
+    if(i>=0){
+        int j=n-1;
+        while(arr[j]<=arr[i]) j--;
+        swap(arr[i], arr[j]);
+    }
+    reverse(arr.begin()+1+i, arr.end());
+}
+
+int main(){
+    vector<int> arr =  {2, 1, 3};
+    nextPermutation(arr);
+
+    for(int x : arr) cout<<x<<" ";
+    cout<<endl;
+
+    return 0;
+}
