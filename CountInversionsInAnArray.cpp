65b3ff2548c70ecd9c918fe6fdb8308faf9fc50a
sanirenic <sanjanameena1904@gmail.com>
2025-08-15 23:07:02 +0530

    GridsUniquePathtwocpp

--- CountInversionsInAnArray.cpp
+++ CountInversionsInAnArray.cpp
@@ -1,61 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int merge(vector<int> &arr, int low, int mid, int high){
+int merge(vector<int> &arr, int low, int mid, int high) {
     vector<int> temp;
-    int left=low;
+    int left = low;
+    int r = mid + 1;  // renamed from right → r
+    int cnt = 0;
 
-    int right==mid+1;
-    int cnt=0;
-
-    while(left<=mid && right<=high){
-        if(arr[left]<=arr[right]){
+    while (left <= mid && r <= high) {
+        if (arr[left] <= arr[r]) {
             temp.push_back(arr[left]);
             left++;
-        }
-        else{
-            temp.push_back(arr[right]);
-            cnt+=(mid-left+1);
-            right++;
+        } else {
+            temp.push_back(arr[r]);
+            cnt += (mid - left + 1);
+            r++;
         }
     }
-    while(left<=mid){
+    while (left <= mid) {
         temp.push_back(arr[left]);
         left++;
     }
-    while(right<=mid){
-        temp.push_back(arr[right]);
-        right++;
+    while (r <= high) {
+        temp.push_back(arr[r]);
+        r++;
     }
 
-    for(int i=low; i<=high; i++){
-        arr[i]=temp[i-low];
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
     }
     return cnt;
 }
 
-int mergeSort(vector<int> &arr, int low, int high){
-    int cnt=0;
-    if(low>=high) return cnt;
-    int mid=(low+high)/2;
+int mergeSort(vector<int> &arr, int low, int high) {
+    int cnt = 0;
+    if (low >= high) return cnt;
+    int mid = (low + high) / 2;
     
-    cnt+=mergeSort(arr, low, mid);
-    cnt+=mergeSort(arr, mid+1, high);
-    cnt+=merge(arr, low, mid, high);
+    cnt += mergeSort(arr, low, mid);
+    cnt += mergeSort(arr, mid + 1, high);
+    cnt += merge(arr, low, mid, high);
 
     return cnt;
 }
 
-int numberOfInversions(vector<int> &a, int n){
-    return mergeSort(a, 0, n-1);
+int numberOfInversions(vector<int> &a, int n) {
+    return mergeSort(a, 0, n - 1);
 }
 
-int main()
-{
+int main() {
     vector<int> a = {5, 4, 3, 2, 1};
     int n = 5;
     int cnt = numberOfInversions(a, n);
-    cout << "The number of inversions are: "
-         << cnt << endl;
+    cout << "The number of inversions are: " << cnt << endl;
     return 0;
 }
