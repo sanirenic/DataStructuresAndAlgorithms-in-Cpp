ad0e373ad170cb6bf19d4ad4e8150286245e5481
sanirenic <sanjanameena1904@gmail.com>
2025-08-19 15:54:43 +0530

    AddTwoNumRepresentesInLLcppp

---
+++ AddTwoNumRepresentesInLL.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution{
+    public:
+    listNode* addTwoNumbers(ListNode* l1, ListNode* l2){
+        ListNode *dummy=new ListNode();
+        ListNode *temp=dummy;
+
+        int carry=0;
+        while(l1!=NULL || l2!=NULL || carry){
+            int sum=0;
+            if(l1!=NULL){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2!=NULL){
+                sum+=l1->val;
+                l2=l2->next;
+            }
+            sum+=carry;
+            carry=sum/10;
+
+            ListNode* node=new ListNode(sum%10);
+            temp->next=node;
+            temp=temp->next;
+        }
+        return dummy->next;
+    }
+};
+
+ListNode* buildList(vector<int> nums) {
+    if(nums.empty()) return nullptr;
+    ListNode* head = new ListNode(nums[0]);
+    ListNode* current = head;
+    for(int i = 1; i < nums.size(); i++) {
+        current->next = new ListNode(nums[i]);
+        current = current->next;
+    }
+    return head;
+}
+
+void printList(ListNode* head) {
+    while(head) {
+        cout << head->val;
+        if(head->next) cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    
+    vector<int> num1 = {2,4,3}; 
+    vector<int> num2 = {5,6,4}; 
+    
+    ListNode* l1 = buildList(num1);
+    ListNode* l2 = buildList(num2);
+
+    Solution sol;
+    ListNode* result = sol.addTwoNumbers(l1, l2);
+
+    cout << "Result: ";
+    printList(result); 
+
+    
+    vector<int> n1 = {9,9,9,9,9,9,9};
+    vector<int> n2 = {9,9,9,9};   
+    
+    l1 = buildList(n1);
+    l2 = buildList(n2);
+
+    result = sol.addTwoNumbers(l1, l2);
+
+    cout << "Result: ";
+    printList(result); 
+
+    return 0;
+}
