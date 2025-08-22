7138dc7e0ef2d65cde7a29d74440ec04a9d7eaec
sanirenic <sanjanameena1904@gmail.com>
2025-08-22 19:11:14 +0530

    SearchSingleElemInSortedArray2cppcpp

--- SearchSingleElemInSortedArray.cpp
+++ SearchSingleElemInSortedArray.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 int singleNonDuplicate(vector<int>& arr) {
-    int n = arr.size(); //size of the array.
+    int n = arr.size(); 
 
-    //Edge cases:
     if (n == 1) return arr[0];
     if (arr[0] != arr[1]) return arr[0];
     if (arr[n - 1] != arr[n - 2]) return arr[n - 1];
@@ -13,25 +12,21 @@ int singleNonDuplicate(vector<int>& arr) {
     while (low <= high) {
         int mid = (low + high) / 2;
 
-        //if arr[mid] is the single element:
         if (arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1]) {
             return arr[mid];
         }
 
-        //we are in the left:
         if ((mid % 2 == 1 && arr[mid] == arr[mid - 1])
                 || (mid % 2 == 0 && arr[mid] == arr[mid + 1])) {
-            //eliminate the left half:
+            
             low = mid + 1;
         }
-        //we are in the right:
+
         else {
-            //eliminate the right half:
             high = mid - 1;
         }
     }
 
-    // dummy return statement:
     return -1;
 }
 
