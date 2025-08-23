4f140bf5a2d0df205627cc2ad11da0f6b1151760
sanirenic <sanjanameena1904@gmail.com>
2025-08-23 15:45:53 +0530

    KthElemOfTwoSortedArraycppcpp

--- KthElemOfTwoSortedArray.cpp
+++ KthElemOfTwoSortedArray.cpp
@@ -5,14 +5,13 @@ using namespace std;
 int kthElement(vector<int> &a, vector<int>& b, int m, int n, int k) {
     if (m > n) return kthElement(b, a, n, m, k);
 
-    int left = k; //length of left half
+    int left = k; 
 
-    //apply binary search:
     int low = max(0, k - n), high = min(k, m);
     while (low <= high) {
         int mid1 = (low + high) >> 1;
         int mid2 = left - mid1;
-        //calculate l1, l2, r1 and r2;
+
         int l1 = INT_MIN, l2 = INT_MIN;
         int r1 = INT_MAX, r2 = INT_MAX;
         if (mid1 < m) r1 = a[mid1];
@@ -24,11 +23,10 @@ int kthElement(vector<int> &a, vector<int>& b, int m, int n, int k) {
             return max(l1, l2);
         }
 
-        //eliminate the halves:
         else if (l1 > r2) high = mid1 - 1;
         else low = mid1 + 1;
     }
-    return 0; //dummy statement
+    return 0; 
 
 }
 
