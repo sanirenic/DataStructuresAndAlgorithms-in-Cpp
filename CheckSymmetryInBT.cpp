90909de4d0bda317780b243aeca5207e72c3df6f
sanirenic <sanjanameena1904@gmail.com>
2025-09-13 15:50:34 +0530

    CheckSymmetryInBTcppcppcpp

--- CheckSymmetryInBT.cpp
+++ CheckSymmetryInBT.cpp
@@ -25,15 +25,8 @@ public:
 };
 
 
-// Helper function to create a sample tree
 TreeNode* buildSampleTree() {
-    /*
-           1
-         /   \
-        2     2
-       / \   / \
-      3   4 4   3
-    */
+   
     TreeNode* root = new TreeNode(1);
     root->left = new TreeNode(2);
     root->right = new TreeNode(2);
