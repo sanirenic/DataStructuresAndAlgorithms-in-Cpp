440629e51c698f29a69b69334068c6ca92a501fe
sanirenic <sanjanameena1904@gmail.com>
2025-09-13 15:50:00 +0530

    CheckSymmetryInBTcpppcpp

--- CheckSymmetryInBT.cpp
+++ CheckSymmetryInBT.cpp
@@ -23,3 +23,35 @@ public:
         return isMirror(root->left, root->right);
     }
 };
+
+
+// Helper function to create a sample tree
+TreeNode* buildSampleTree() {
+    /*
+           1
+         /   \
+        2     2
+       / \   / \
+      3   4 4   3
+    */
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(2);
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+    root->right->left = new TreeNode(4);
+    root->right->right = new TreeNode(3);
+    return root;
+}
+
+int main() {
+    Solution sol;
+
+    TreeNode* root = buildSampleTree();
+    if (sol.isSymmetric(root))
+        cout << "The tree is symmetric ✅" << endl;
+    else
+        cout << "The tree is NOT symmetric ❌" << endl;
+
+    return 0;
+}
