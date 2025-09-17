76ad090fd2aef1f453a4a88bb2abd57e94896ffb
sanirenic <sanjanameena1904@gmail.com>
2025-09-17 16:37:55 +0530

    ConstructBSTfromPreorderTraversalcppcpp

--- ConstructBSTfromPreorderTraversal.cpp
+++ ConstructBSTfromPreorderTraversal.cpp
@@ -3,7 +3,6 @@
 #include <climits>
 using namespace std;
 
-// TreeNode structure
 struct TreeNode {
     int val;
     TreeNode* left;
@@ -11,25 +10,20 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-// Solution class to build BST from preorder
 class Solution {
 public:
     int idx = 0;
 
     TreeNode* buildBST(vector<int>& preorder, long long minVal, long long maxVal) {
-        // Base case: if all nodes are processed
+       
         if (idx >= preorder.size()) return NULL;
 
         int val = preorder[idx];
-
-        // If current value doesn’t fit in range, return NULL
         if (val < minVal || val > maxVal) return NULL;
 
-        // Create root node
         TreeNode* root = new TreeNode(val);
         idx++;
 
-        // Recursively build left and right subtrees
         root->left = buildBST(preorder, minVal, val - 1LL);
         root->right = buildBST(preorder, val + 1LL, maxVal);
 
@@ -41,7 +35,6 @@ public:
     }
 };
 
-// Utility function: inorder traversal to check correctness
 void inorderTraversal(TreeNode* root) {
     if (!root) return;
     inorderTraversal(root->left);
@@ -49,7 +42,6 @@ void inorderTraversal(TreeNode* root) {
     inorderTraversal(root->right);
 }
 
-// Utility function: preorder traversal to confirm reconstruction
 void preorderTraversal(TreeNode* root) {
     if (!root) return;
     cout << root->val << " ";
